b8d1dd52338a6cf8eb4b1bb04782cfe2fbe902b7
Ben Foxall <ben.foxall@oxbotica.com>
2020-03-28 11:44:50 +0000

    WS & storage!

--- poker.js
+++ poker.js
@@ -1,7 +1,43 @@
+const http = require('http');
 const express = require('express')
+const WebSocket = require('ws');
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const storage = require('./lib/storage');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+console.log("X:", storage)
+
+app.use('/', express.static('www'))
+
+app.get('/new', function (req, res) {
+    var sid = storage.newSession();
+    res.redirect('/sessions/' + sid);
+});
+
+app.get('/sessions/:sessionid', function (req, res) {
+    res.send(storage.showCaseSession(req.params.sessionid))
+});
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws) {
+    console.log("WS: Conneted!")
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.send('something');
+});
+
+server.listen(port, (err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log(`Probably listening on ${port}`)
+})
